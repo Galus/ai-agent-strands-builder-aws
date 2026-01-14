8bb20047db9389fd3a08f4d8fafe343b48f421a2
Galus <mgalus@gmail.com>
2026-01-14 16:01:19 -0500

    Working with AWS

--- src/ai_agent_strands_builder_aws/app.py
+++ src/ai_agent_strands_builder_aws/app.py
@@ -1,5 +1,10 @@
+
+from ai_agent_strands_builder_aws.core.agent import run
+
 def main():
     print("Hello from ai-agent-strands-builder-aws!")
+
+    run()
 
 
 if __name__ == "__main__":
---
+++ src/ai_agent_strands_builder_aws/core/agent.py
@@ -0,0 +1,7 @@
+from strands import Agent
+
+def run():
+    agent = Agent()
+    resp = agent("Explain Amazon Bedrock Agents")
+    print(resp)
+
